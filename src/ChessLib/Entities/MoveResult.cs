558253f3e7cebc474ece7be6eaf5e23138757647
Dart <rahmatulla.r08@gmail.com>
2026-04-07 00:25:50 +0300

    remove unnecessary set accessor

--- src/ChessLib/Entities/MoveResult.cs
+++ src/ChessLib/Entities/MoveResult.cs
@@ -2,6 +2,6 @@ namespace ChessLib.Entities;
 
 public class MoveResult(bool isValid, TerminalPositionType? type = null)
 {
-    public bool IsValid { get; private set; } = isValid;
-    public TerminalPositionType? TerminalType { get; private set; } = type;
+    public bool IsValid { get; } = isValid;
+    public TerminalPositionType? TerminalType { get; } = type;
 }
