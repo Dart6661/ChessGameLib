768deed3865a0eba5f6445061683b43a7e1a2778
Dart <rahmatulla.r08@gmail.com>
2026-04-06 15:08:52 +0300

    remove unnecessary exceptions

--- src/ChessLib/Exceptions/GameExceptions.cs
+++
@@ -1,5 +0,0 @@
-namespace ChessLib.Exceptions;
-
-public class CheckMateException(string message) : Exception(message);
-
-public class StaleMateException(string message) : Exception(message);
--- src/ChessLib/Exceptions/MoveExceptions.cs
+++ src/ChessLib/Exceptions/MoveExceptions.cs
@@ -1,5 +1,3 @@
 namespace ChessLib.Exceptions;
 
-public class MovementException(string message) : Exception(message);
-
 public class ReplacementException(string message) : Exception(message);
